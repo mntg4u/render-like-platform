c25456294a81a5facdf4723707a80d13062b8902
MN - TG <mntg3509@gmail.com>
2024-11-25 21:44:26 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+import ServiceCreate from "./components/ServiceCreate";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create" element={<ServiceCreate />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
